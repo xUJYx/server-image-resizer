0cc4605c435168a02412480a7abee8690beb373b
UJy <ujy@ukr.net>
2017-01-18 09:17:03 +0200

    +deleted some trash from code *added work process view while copying from resized to completed

--- img-resizer.php
+++ img-resizer.php
@@ -9,58 +9,6 @@ if(in_array($_SERVER['REMOTE_ADDR'], Array('94.244.17.6')))
     error_reporting(E_ALL);
 }
 
-
-//find_files('./img');
-
-// removes files and non-empty directories
-function rrmdir($dir) {
-    if (is_dir($dir)) {
-        $files = scandir($dir);
-        foreach ($files as $file)
-            if ($file != "." && $file != "..") rrmdir("$dir/$file");
-//        rmdir($dir);
-    }
-    else if (file_exists($dir)) /*unlink($dir)*/;
-}
-
-// copies files and non-empty directories
-function rcopy($src, $dst) {
-    if (file_exists($dst)) {echo "directory {$dst} already exists \n"; /* rrmdir($dst) */;}
-    if (is_dir($src))
-    {
-        echo "Создаю директорию {$dst}\n";
-        mkdir($dst);
-        $files = scandir($src);
-        foreach ($files as $file)
-            if ($file != "." && $file != "..") rcopy("$src/$file", "$dst/$file");
-    }
-    elseif (file_exists($src))
-    {
-        echo "делаю копию {$src} в {$dst}\n";
-        copy($src, $dst);
-    }
-}
-/*function find_files($seed)
-{
-    if(! is_dir($seed)) return false;
-    $files = array();
-    $dirs = array($seed);
-    while(NULL !== ($dir = array_pop($dirs)))
-    {
-        if($dh = opendir($dir))
-        {
-            while( false !== ($file = readdir($dh)))
-            {
-                if($file == '.' || $file == '..') continue;
-                $path = $dir . '/' . $file;
-                var_dump($dir);
-
-                if(is_dir($path)) { $dirs[] = $path; }
-            }
-            closedir($dh);
-        }
-    }
-}*/
 function find_files($seed, $dir_for_resizing)
 {
     if(!is_dir($seed)) return false;
@@ -184,27 +132,7 @@ function getWorkData ($resized_directory = "testing", $dir_to_search = "img", $d
         {
             $resized_images_list = resizedImagesList($dir_to_resize); // дальше ищем эллементы этого списка в $imagess_path при помощи array_intersect($imagess_path, $resized_images_list). При совпадении в ключах будем иметь значения совпавших ключей - по ключам копируем эту картинку по новому пути.
             $renamed_list_to_copy = array_intersect($renamed_images_name_list, $resized_images_list);
-//var_dump(array_unique($resized_images_list));die();
-                /*$found = array();
-                $not_found = array();
 
-            foreach ($resized_images_list as $resized_image)
-            {
-                if (!in_array($resized_image, $renamed_images_name_list))
-                {
-                    $not_found[] = "Элемент $resized_image не найден \r\n";
-                } elseif (in_array($resized_image, $renamed_images_name_list))
-                {
-                    $found[] = "Элемент $resized_image найден \r\n";
-                }
-            }
-            var_dump($not_found);
-            echo "\r\n-----------------------------------------------------\r\n";
-            echo "-----------------------------------------------------\r\n";
-            echo "-----------------------------------------------------\r\n";
-            var_dump($found);*/
-
-//die();
             if (createDirectories($images_directories, $dir_to_resize, $resized_directory)) echo "<span style=\"font-weight:bold; color:red;\">Все директории успешно созданы</span> <br />\r\n";
 
             //var_dump($images_to_resize_path);
@@ -253,9 +181,7 @@ function createDirectories($images_directories, $dir_to_resize, $resized_dir)
         $piece_of_dir_resized = $resized_dir;
         foreach ($piece_of_dir as $key => $value)
         {
-//            $piece_of_dir_to_resize .= $value . "/";
             $piece_of_dir_resized .= $value . "/";
-//            if(!is_dir($piece_of_dir_to_resize) && !mkdir($piece_of_dir_to_resize)) echo "НЕ МОГУ СОЗДАТЬ ПАПКУ \"$piece_of_dir_to_resize\" !!!\r\n";
             if(!is_dir($piece_of_dir_resized) && !mkdir($piece_of_dir_resized)) echo "НЕ МОГУ СОЗДАТЬ ПАПКУ \"$piece_of_dir_resized\" !!!\r\n";
         }
     }
@@ -268,7 +194,7 @@ function copyImgWithStructure ($images_path, $renamed_images_name_list, $dir_to_
     $images_resized_path = "./" . $dir_to_resize . "/" . $renamed_images_name_list;
     $images_new_path = str_replace("./", "./". $resized_directory ."/", $images_path);
 //    var_dump($images_new_path); die();
-    if(!copy($images_resized_path, $images_new_path))
+    if(copy($images_resized_path, $images_new_path))
     {
         echo "Успешно скопирован файл [{$file_copy_counter}] из $images_resized_path в => $images_new_path \r\n";
         createCSV ($file_copy_counter, $images_resized_path, $images_new_path, 'work-done-list.csv');
@@ -278,7 +204,6 @@ function copyImgWithStructure ($images_path, $renamed_images_name_list, $dir_to_
     }
     $file_copy_counter++;
 }
-//mkdir("./Big_Words");
 
 
 getWorkData("images-resize-completed", "img", "images-for-resize"); // Папка куда ложим файлы, папка где ищем картинки, папка откуда подтягиваем обработанные картинки
